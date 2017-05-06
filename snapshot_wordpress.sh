b62db7c2f2717893a1401da3f264262b6cf8d6e8
Jason <jasonventresca@gmail.com>
2017-05-06 21:04:19 +0000

    uncommented tar part

--- snapshot_wordpress.sh
+++ snapshot_wordpress.sh
@@ -9,15 +9,15 @@ WP_DBS_DUMP_FILE="$HOME/sql_dump.sql"
 echo "Dumping MySQL databases..."
 mysqldump --all-databases $MYSQL_CREDS >$WP_DBS_DUMP_FILE
 
-# echo "Archiving public_html directory..."
-# rm -f public_html.tar.gz
-# tar cf public_html{.tar,}
-# gzip -f public_html.tar
-# 
-# echo "Please copy the backup files to Google Drive or somewhere safe:"
-# for bak_file in public_html.tar.gz $WP_DBS_DUMP_FILE ; do
-#     echo "    $(ls -lh $bak_file)"
-# done
+echo "Archiving public_html directory..."
+rm -f public_html.tar.gz
+tar cf public_html{.tar,}
+gzip -f public_html.tar
+
+echo "Please copy the backup files to Google Drive or somewhere safe:"
+for bak_file in public_html.tar.gz $WP_DBS_DUMP_FILE ; do
+    echo "    $(ls -lh $bak_file)"
+done
 
 echo
 echo "Thanks, bye :)"
